fd57a2b4d2a588234e0909e904af938bcd8e309a
Marina A <25265018+MarinaBSA@users.noreply.github.com>
2020-10-25 22:07:42 +0100

    added function to fetch media info from api

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const searchBar = document.querySelector('.search-bar');
+const apiURL = 'http://www.omdbapi.com/?apikey=b78d8af3&';
+const Http = new XMLHttpRequest();
+let apiRequestResult;
 var mediaName;
 
 
 searchBar.addEventListener('keydown', event => {
   if(event.keyCode == 13) {
     mediaName = searchBar.value;
-    //console.log(mediaName);
+    getMediaInformationFromAPIWithTitle
+    apiRequestResult = getMediaInformationFromAPIWithTitle(mediaName);
   }
 });
+
+
+function getMediaInformationFromAPIWithTitle(mediaName) {
+  let urlWithQuery = apiURL + 't=' + mediaName;
+  Http.open("GET", urlWithQuery);
+  Http.send();
+
+  Http.onreadystatechange = function(){
+    if (this.readyState==4 && this.status==200) {
+      console.log(Http.responseText);
+      return Http.responseText;
+    }
+  }
+}
+
+
